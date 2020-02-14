7590d8492d57c853f2491f72fd50877c1f3bf599
Ilian Corneliussen <ilianc@kth.se>
2020-02-14 14:47:14 +0100

    Working

--- Lab2/LAB2.py
+++ Lab2/LAB2.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
 
-
-# GENERATE TESTING DATA:
+global ALPHA_NONZERO, targets_NONZERO, b, inputs_NONZERO
 classA = np.concatenate(
     (np.random.randn(10, 2) * 0.2 + [1.5, 0.5],
     np.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
@@ -14,7 +13,7 @@ classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
 inputs = np.concatenate((classA, classB))
 targets = np.concatenate(
     (np.ones(classA.shape[0]),
-    np.ones(classB.shape[0])))
+    -np.ones(classB.shape[0])))
 
 N = inputs.shape[0]
 
@@ -23,14 +22,12 @@ random.shuffle(permute)
 inputs = inputs[permute, : ]
 targets = targets[permute]
 
-
 def linearKernel(x,y):
     '''
     Linear Kernel:
     This kernel simply returns the scalar product between the two points.
     This results in a linear separation.
     '''
-    
     return np.dot(x, y)
 
 def polynomialKernel(x,y):
@@ -41,8 +38,8 @@ def polynomialKernel(x,y):
     quadratic shapes (ellipses, parabolas, hyperbolas). Setting p = 3 or higher
     will result in more complex shapes.
     '''
-    p = 2
-    return (np.dot(x.transpose(),y) + 1)**p
+    p = 5
+    return (np.dot(x,y) + 1)**p
 
 def RBF(x,y):
     '''
@@ -51,44 +48,32 @@ def RBF(x,y):
     and often results in very good boundaries. The parameter σ is used to
     control the smoothness of the boundary.
     '''
-    sigma = 0.5
-    return math.exp( -(np.linalg.norm(x, y)^2) / (2*sigma**2) )
+    sigma = 2
+    return math.exp( -(np.linalg.norm(x, y)**2) / (2*sigma**2) )
 
-global ALPHA_NONZERO, X_NONZERO, Y_NONZERO, T_NONZERO, B 
 
-KERNEL = polynomialKernel
-T = targets
-X = inputs[0,:]
-Y = inputs[1,:]
-P_MATRIX = np.matrix(N,N)
+KERNEL = RBF
+P_MATRIX = np.zeros((N,N))
 for i in range(N):
     for j in range(N):
-        P_MATRIX(i,j) = T[i]*T[j]*KERNEL(inputs[i], inputs[j])
+        P_MATRIX[i,j] = targets[i] * targets[j] * linearKernel(inputs[i], inputs[j])
 
 
 def objectiveFunction(alpha):
-    return 0.5 * np.dot( np.dot(alpha, alpha), P_MATRIX ) - np.sum(alpha)
+    return (1/2) * np.dot(alpha, np.dot(alpha, P_MATRIX ) ) - np.sum(alpha)
 
 def zerofun(alpha):
-    return np.dot(alpha, T)
+    return np.dot(alpha, targets)
 
 def indicator(x, y):
-    global ALPHA_NONZERO, T_NONZERO, B
-    return np.sum(np.dot(np.dot(ALPHA_NONZERO, T_NONZERO), KERNEL(x, y))) - B
+    global ALPHA_NONZERO, targets_NONZERO, b, inputs_NONZERO
+    ind = 0
+    for i in range(np.size(ALPHA_NONZERO)):
+        ind += ALPHA_NONZERO[i] * targets_NONZERO[i] * KERNEL((x,y), inputs_NONZERO[i])
+    return ind - b
 
 def main():
-    global ALPHA_NONZERO, T_NONZERO, B
-    '''
-    TODO: Suitable Kernel Function              [X]
-    TODO: Implement function objective          [X]
-    TODO: Implement function zerofun            [X]
-    TODO: Call minimize                         [X]
-    TODO: Extract the non-zero Alpha values     [X]
-    TODO: Calculate the b value using eq. (7)   [X]
-    TODO: Implement the indicator function      [X]
-
-    TODO: Genereate test data for visualization and results. 
-    '''
+    global ALPHA_NONZERO, targets_NONZERO, b, inputs_NONZERO
 
     # CALL MINIMIZE:
     C = 10
@@ -103,19 +88,19 @@ def main():
     # EXTRACT NON-ZERO ALPHA VALUES:
     index = np.where(alpha > 10**(-5))
 
-    X_NONZERO = X[index]
-    Y_NONZERO = Y[index]
-    T_NONZERO = T[index]
+
+    inputs_NONZERO = inputs[index]
+    targets_NONZERO = targets[index]
     ALPHA_NONZERO = alpha[index]
     
     # CALCULATE THE b VALUE USING EQ. (7):
-    # B = 0
-    # for i in range(len(ALPHA_NONZERO)):
-    #     B += ALPHA_NONZERO[i]* T_NONZERO[i]*KERNEL(X_NONZERO,Y_NONZERO)
-    # B = B - T_NONZERO[0]
-    B = np.sum( np.dot(ALPHA_NONZERO, np.dot(T_NONZERO, KERNEL(X_NONZERO,Y_NONZERO) ) ) ) - T_NONZERO[0]
-    print(B)
+    b = 0
+    for i in range(len(ALPHA_NONZERO)):
+        b += ALPHA_NONZERO[i]* targets_NONZERO[i]*KERNEL(inputs_NONZERO[0],inputs_NONZERO[i])
+    b = b - targets_NONZERO[0]
+    
 
+    # PLOTTING
     plt.plot([p[0] for p in classA],
              [p[1] for p in classA], 
              'b.')
@@ -130,7 +115,8 @@ def main():
     grid = np.array( [[indicator(x, y)
                         for x in xgrid]
                         for y in ygrid])
-    plt.contour(xgrid,ygrid,grid, (-1.0, 0.0, 1.0),
+    plt.contour(xgrid,ygrid,grid, 
+                (-1.0, 0.0, 1.0),
                 colors = ('red','black', 'blue'),
                 linewidths = (1,3,1))
  
