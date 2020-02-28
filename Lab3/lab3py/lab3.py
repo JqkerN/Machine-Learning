5e1fd93a43c63fc404e062bdae4be62fd9b4bf10
Ilian Corneliussen <ilianc@kth.se>
2020-02-28 14:23:03 +0100

    FINNISHED: WOOOHOOO

--- Lab3/lab3py/lab3.py
+++ Lab3/lab3py/lab3.py
@@ -180,9 +180,9 @@ class BayesClassifier(object):
 # Call `genBlobs` and `plotGaussian` to verify your estimates.
 
 
-X, labels = genBlobs(centers=5)
-mu, sigma = mlParams(X,labels)
-plotGaussian(X,labels,mu,sigma)
+# X, labels = genBlobs(centers=5)
+# mu, sigma = mlParams(X,labels)
+# plotGaussian(X,labels,mu,sigma)
 
 
 # Call the `testClassifier` and `plotBoundary` functions for this part.
@@ -228,10 +228,12 @@ def trainBoost(base_classifier, X, labels, T=10):
 
         # TODO: Fill in the rest, construct the alphas etc.
         # ==========================
-
-        error = np.sum(wCur[np.where(vote == labels, 1, 0)], axis = 1)
+        wCur = wCur.reshape(1,-1).reshape(-1)
+        error = np.dot(wCur, 1 - np.where(vote == labels, 1, 0))
         alpha = 0.5*(np.log(1-error) - np.log(error))
-        wCur = wCur/np.sum(wCur) * np.where(vote == labels, np.exp(-alpha), np.exp(alpha))
+        wCur = wCur * np.where(vote == labels, np.exp(-alpha), np.exp(alpha))
+        wCur = wCur/np.sum(wCur)
+        wCur = wCur.reshape(-1,1)
         alphas.append(alpha) # you will need to append the new alpha
     
         # ==========================
@@ -257,8 +259,11 @@ def classifyBoost(X, classifiers, alphas, Nclasses):
         # TODO: implement classificiation when we have trained several classifiers!
         # here we can do it by filling in the votes vector with weighted votes
         # ==========================
-        for k in range(Nclasses):
-            votes[:,k] = np.dot(alpha*np.where(classifiers.classify(X) == k))
+        t = 0
+        for clasifier in classifiers:
+            for k in range(Nclasses):
+                votes[:,k] += (alphas[t]*np.where(clasifier.classify(X) == k,1,0))
+            t += 1
         
         # ==========================
 
@@ -292,71 +297,42 @@ class BoostClassifier(object):
 # Call the `testClassifier` and `plotBoundary` functions for this part.
 
 
-testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='iris',split=0.7)
+# testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='iris',split=0.7)
 
 
 
-#testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='vowel',split=0.7)
+# testClassifier(BoostClassifier(BayesClassifier(), T=10), dataset='vowel',split=0.7)
 
 
 
-plotBoundary(BoostClassifier(BayesClassifier()), dataset='iris',split=0.7)
+# plotBoundary(BoostClassifier(BayesClassifier()), dataset='iris',split=0.7)
 
 
 # Now repeat the steps with a decision tree classifier.
 
 
-#testClassifier(DecisionTreeClassifier(), dataset='iris', split=0.7)
+testClassifier(DecisionTreeClassifier(), dataset='iris', split=0.7)
 
 
 
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
+testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
 
 
 
-#testClassifier(DecisionTreeClassifier(), dataset='vowel',split=0.7)
+testClassifier(DecisionTreeClassifier(), dataset='vowel',split=0.7)
 
 
 
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='vowel',split=0.7)
+testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='vowel',split=0.7)
 
 
 
-#plotBoundary(DecisionTreeClassifier(), dataset='iris',split=0.7)
+plotBoundary(DecisionTreeClassifier(), dataset='iris',split=0.7)
 
 
 
-#plotBoundary(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
-
-
-# ## Bonus: Visualize faces classified using boosted decision trees
-# 
-# Note that this part of the assignment is completely voluntary! First, let's check how a boosted decision tree classifier performs on the olivetti data. Note that we need to reduce the dimension a bit using PCA, as the original dimension of the image vectors is `64 x 64 = 4096` elements.
-
-
-#testClassifier(BayesClassifier(), dataset='olivetti',split=0.7, dim=20)
+plotBoundary(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='iris',split=0.7)
 
 
 
-#testClassifier(BoostClassifier(DecisionTreeClassifier(), T=10), dataset='olivetti',split=0.7, dim=20)
-
-
-# You should get an accuracy around 70%. If you wish, you can compare this with using pure decision trees or a boosted bayes classifier. Not too bad, now let's try and classify a face as belonging to one of 40 persons!
-
-
-#X,y,pcadim = fetchDataset('olivetti') # fetch the olivetti data
-#xTr,yTr,xTe,yTe,trIdx,teIdx = trteSplitEven(X,y,0.7) # split into training and testing
-#pca = decomposition.PCA(n_components=20) # use PCA to reduce the dimension to 20
-#pca.fit(xTr) # use training data to fit the transform
-#xTrpca = pca.transform(xTr) # apply on training data
-#xTepca = pca.transform(xTe) # apply on test data
-# use our pre-defined decision tree classifier together with the implemented
-# boosting to classify data points in the training data
-#classifier = BoostClassifier(DecisionTreeClassifier(), T=10).trainClassifier(xTrpca, yTr)
-#yPr = classifier.classify(xTepca)
-# choose a test point to visualize
-#testind = random.randint(0, xTe.shape[0]-1)
-# visualize the test point together with the training points used to train
-# the class that the test point was classified to belong to
-#visualizeOlivettiVectors(xTr[yTr == yPr[testind],:], xTe[testind,:])
 
